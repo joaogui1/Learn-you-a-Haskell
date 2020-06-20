bdf69adcdfc04e5e64055f430a045f7bcc648f8a
joaogui1 <joaoguilhermearujo@gmail.com>
2020-06-20 09:50:55 -0300

    control monads

--- chapter9.hs
+++ chapter9.hs
@@ -13,3 +13,21 @@ main = do
     let bigFirstName = map toUpper firstName  
         bigLastName = map toUpper lastName  
     putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?" 
+
+import Control.Monad   
+  
+main = do  
+    c <- getChar  
+    when (c /= ' ') $ do  
+        putChar c  
+        main  
+
+--main = do sequence (map print [1,2,3,4,5])
+-- mapM_ print [1,2,3,4,5], no result
+-- mapM print [1,2,3,4,5], has result
+
+-- forever, the infinite loop monad
+--   main = forever $ do  
+--     putStr "Give me some input: "  
+--     l <- getLine  
+--     putStrLn $ map toUpper l  
