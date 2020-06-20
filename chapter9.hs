287cb86501c58acaead02b9d22d52dc6da6a61d2
joaogui1 <joaoguilhermearujo@gmail.com>
2020-06-20 10:11:30 -0300

    file IO

--- chapter9.hs
+++ chapter9.hs
@@ -31,3 +31,58 @@ main = do
 --     putStr "Give me some input: "  
 --     l <- getLine  
 --     putStrLn $ map toUpper l  
+
+-- getContents: Lazyly read stdin until EOF
+-- main = do  
+--     contents <- getContents  
+--     putStr (map toUpper contents)  
+
+-- main = do  
+--     contents <- getContents  
+--     putStr (shortLinesOnly contents)  
+  
+-- shortLinesOnly :: String -> String  
+-- shortLinesOnly input =   
+--     let allLines = lines input  
+--         shortLines = filter (\line -> length line < 10) allLines  
+--         result = unlines shortLines  
+--     in  result  
+
+-- Interact takes a String -> String function, and returns an I/O action that will take some input, 
+-- run that function on it and then print out the function's result
+-- main = interact shortLinesOnly  
+  
+-- shortLinesOnly :: String -> String  
+-- shortLinesOnly input =   
+--     let allLines = lines input  
+--         shortLines = filter (\line -> length line < 10) allLines  
+--         result = unlines shortLines  
+--     in  result  
+
+-- Short version
+-- main = interact $ unlines . filter ((<10) . length) . lines  
+
+
+-- import System.IO  
+  
+-- main = do  
+--     handle <- openFile "girlfriend.txt" ReadMode  
+--     contents <- hGetContents handle  
+--     putStr contents  
+--     hClose handle  
+
+
+-- import System.IO     
+    
+-- main = do     
+--     withFile "girlfriend.txt" ReadMode (\handle -> do  
+--         contents <- hGetContents handle     
+--         putStr contents)  
+
+-- File to string manipulation
+-- import System.IO     
+-- import Data.Char  
+    
+-- main = do     
+--     contents <- readFile "girlfriend.txt"     
+--     writeFile "girlfriendcaps.txt" (map toUpper contents)  
